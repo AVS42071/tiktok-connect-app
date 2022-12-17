43c807b391ed2401031ed2cce286d28a73e6f02b
AVS42071 <96913756+AVS42071@users.noreply.github.com>
2022-12-17 15:50:53 -0600

    built the connector server

--- server.js
+++ server.js
@@ -4,22 +4,103 @@ const { WebcastPushConnection } = require('tiktok-live-connector');
 let tiktokUsername = "officialgeilegisela";
 
 // Create a new wrapper object and pass the username
-let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
-// Connect to the chat (await can be used as well)
-tiktokLiveConnection.connect().then(state => {
-    console.info(`Connected to roomId ${state.roomId}`);
-}).catch(err => {
-    console.error('Failed to connect', err);
+
+let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername, {
+    processInitialData: false,
+    enableExtendedGiftInfo: true,
+    enableWebsocketUpgrade: true,
+    requestPollingIntervalMs: 2000,
+    clientParams: {
+        "app_language": "en-US",
+        "device_platform": "web"
+    },
+    requestHeaders: {
+        "headerName": "headerValue"
+    },
+    websocketHeaders: {
+        "headerName": "headerValue"
+    },
+    requestOptions: {
+        timeout: 10000
+    },
+    websocketOptions: {
+        timeout: 10000
+    }
+});
+tiktokLiveConnection.on('connected', state => {
+    console.log('Hurray! Connected!', state);
 })
 
-// Define the events that you want to handle
-// In this case we listen to chat messages (comments)
-tiktokLiveConnection.on('chat', data => {
-    console.log(`${data.uniqueId} (userId:${data.userId}) writes: ${data.comment}`);
+tiktokLiveConnection.on('disconnected', () => {
+    console.log('Disconnected :(');
 })
 
-// And here we receive gifts sent to the streamer
+
+tiktokLiveConnection.on('streamEnd', (actionId) => {
+    if (actionId === 3) {
+        console.log('Stream ended by user');
+    }
+    if (actionId === 4) {
+        console.log('Stream ended by platform moderator (ban)');
+    }
+})
+
+
+tiktokLiveConnection.on('websocketConnected', websocketClient => {
+    console.log("Websocket:", websocketClient.connection);
+})
+
+
+tiktokLiveConnection.on('error', err => {
+    console.error('Error!', err);
+})
+
+
 tiktokLiveConnection.on('gift', data => {
-    console.log(`${data.uniqueId} (userId:${data.userId}) sends ${data.giftId}`);
+    if (data.giftType === 1 && !data.repeatEnd) {
+        // Streak in progress => show only temporary
+        console.log(`${data.uniqueId} is sending gift ${data.giftName} x${data.repeatCount}`);
+    } else {
+        // Streak ended or non-streakable gift => process the gift with final repeat_count
+        console.log(`${data.uniqueId} has sent gift ${data.giftName} x${data.repeatCount}`);
+    }
+})
+
+tiktokLiveConnection.on('roomUser', data => {
+    console.log(`Viewer Count: ${data.viewerCount}`);
+})
+
+
+tiktokLiveConnection.on('like', data => {
+    console.log(`${data.uniqueId} sent ${data.likeCount} likes, total likes: ${data.totalLikeCount}`);
+})
+
+tiktokLiveConnection.on('subscribe', (data) => {
+    console.log(data.uniqueId, "subscribed!");
+})
+
+tiktokLiveConnection.on('follow', (data) => {
+    console.log(data.uniqueId, "followed!");
+})
+
+
+tiktokLiveConnection.getRoomInfo().then(roomInfo => {
+    console.log(roomInfo);
+    console.log(`Stream started timestamp: ${roomInfo.create_time}, Streamer bio: ${roomInfo.owner.bio_description}`);
+    console.log(`HLS URL: ${roomInfo.stream_url.hls_pull_url}`); // Can be played or recorded with e.g. VLC
+}).catch(err => {
+    console.error(err);
+})
+
+
+let tiktokLiveConnection = new WebcastPushConnection('@username');
+
+tiktokLiveConnection.getAvailableGifts().then(giftList => {
+    console.log(giftList);
+    giftList.forEach(gift => {
+        console.log(`id: ${gift.id}, name: ${gift.name}, cost: ${gift.diamond_count}`)
+    });
+}).catch(err => {
+    console.error(err);
 })
